8d22466ddd7c988edb0a120727bc5fb7992c4e30
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-03-10 17:41:39 +0000

    set up static file serving for css stylesheet

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const router = express.Router();
 
 app.get("/", (req, res) => {
   console.log("Responding to root route")
-  res.sendFile(path.join(__dirname+'/index.html'))
+  res.sendFile(__dirname+'/index.html')
 })
 
+app.get('/public/style.css', (req, res) => {
+  res.sendFile(__dirname + '/public/style.css');
+})
 // localhost:3003
 app.listen(port, () => {
   console.log("Server is listening on 3003...")
