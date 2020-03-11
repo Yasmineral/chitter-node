03684933a53bc9a4f139e812e09c2a26960bc9f9
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-03-11 10:50:40 +0000

    fix static file serving with native express method

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
   console.log("Responding to root route")
   res.sendFile(__dirname+'/index.html')
 })
 
-app.get('/public/style.css', (req, res) => {
-  res.sendFile(__dirname + '/public/style.css');
-})
-// localhost:3003
 app.listen(port, () => {
   console.log("Server is listening on 3003...")
 })
