4fe9184bd38a6173215cef4db4b2499729082b21
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-03-12 10:06:14 +0000

    tidy up home routing on app.js page

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const router = express.Router();
 
 app.use(express.static('public'));
 app.use(express.json());
-
 app.listen(port, () => {
   console.log("Server is listening on 3003...")
 })
 
-app.get("/", (req, res) => {
-  console.log("Responding to root route")
-  res.sendFile(__dirname+'/public/client/index.html')
-})
-
-app.post("/", (req, res) => {
- console.log(req.body)
-})
+app.route('/')
+  .get((req, res) => {
+    console.log("Responding to root route")
+    res.sendFile(__dirname+'/public/client/index.html')
+  })
+  .post((req, res) => {
+    console.log(req.body)
+  })
