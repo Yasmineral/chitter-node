a0f9f6f28682510adcb63399ecfc1a2d85c7942f
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-05-28 08:46:17 +0100

    add jest & babel to project

---
+++ spec/triangle.spec.js
@@ -0,0 +1,10 @@
+import { Triangle } from '../triangle';
+
+describe('Triangle', () => {
+  describe('equilateral triangle', () => {
+    test('all sides are equal', () => {
+      const triangle = new Triangle(2, 2, 2);
+      expect(triangle.isEquilateral()).toBe(true);
+    });
+  });
+});
